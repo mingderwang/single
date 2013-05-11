1f9a89c4eb96f42d42e44f7ed99cfb2ae96e7110
Ming-der Wang <ming@katdc.com>
2013-05-11 17:19:44 +0800

    made JSidePanel on work.

---
+++ single/MySidePanelControllerViewController.h
@@ -0,0 +1,13 @@
+//
+//  MySidePanelControllerViewController.h
+//  single
+//
+//  Created by Wang Ming-der on 5/11/13.
+//  Copyright (c) 2013 Kat Digital Corp. All rights reserved.
+//
+
+#import "JASidePanelController.h"
+
+@interface MySidePanelControllerViewController : JASidePanelController
+
+@end
---
+++ single/MySidePanelControllerViewController.m
@@ -0,0 +1,47 @@
+//
+//  MySidePanelControllerViewController.m
+//  single
+//
+//  Created by Wang Ming-der on 5/11/13.
+//  Copyright (c) 2013 Kat Digital Corp. All rights reserved.
+//
+
+#import "MySidePanelControllerViewController.h"
+
+@interface MySidePanelControllerViewController ()
+
+@end
+
+@implementation MySidePanelControllerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - JASidePanels
+
+-(void) awakeFromNib
+{
+    [self setLeftPanel:[self.storyboard instantiateViewControllerWithIdentifier:@"leftViewController"]];
+    [self setCenterPanel:[self.storyboard instantiateViewControllerWithIdentifier:@"centerViewController"]];
+    [self setRightPanel:[self.storyboard instantiateViewControllerWithIdentifier:@"rightViewController"]];
+}
+
+@end

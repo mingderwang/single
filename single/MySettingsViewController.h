38c78e7cada07f7b5889d02f986ef2a5c9676e3e
Ming-der Wang <ming@katdc.com>
2013-05-16 00:43:29 +0800

    added settings done button to dismiss current modal view.

---
+++ single/MySettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  MySettingsViewController.h
+//  single
+//
+//  Created by Wang Ming-der on 5/15/13.
+//  Copyright (c) 2013 Kat Digital Corp. All rights reserved.
+//
+
+#import "IASKAppSettingsViewController.h"
+
+@interface MySettingsViewController : IASKAppSettingsViewController
+- (IBAction)exit:(id) sender;
+@end
---
+++ single/MySettingsViewController.m
@@ -0,0 +1,42 @@
+//
+//  MySettingsViewController.m
+//  single
+//
+//  Created by Wang Ming-der on 5/15/13.
+//  Copyright (c) 2013 Kat Digital Corp. All rights reserved.
+//
+
+#import "MySettingsViewController.h"
+
+@interface MySettingsViewController ()
+
+@end
+
+@implementation MySettingsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)exit:(id) sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+@end
